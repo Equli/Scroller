08ab4e62582bee48364783a59709aee9a03aba09
Maciej <mtomaszewski006@gmail.com>
2023-03-14 12:23:34 +0100

    Add mobile swiper

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const scroller = new Scroller('#root');
+
     document.addEventListener('wheel', (e) => scroller.listenScroll(e))
+
+    document.addEventListener('swipeUp', () => scroller.scroll(1));
+    document.addEventListener('swipeDown', () => scroller.scroll(-1));
+
+    document.addEventListener('swipeUp', () => console.log('Up'));
+    document.addEventListener('swipeDown', () => console.log('Down'));
 })
 
 
